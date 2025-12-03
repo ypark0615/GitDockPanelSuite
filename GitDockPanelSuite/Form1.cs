e38f9d81d16e8eed596f7f45cf311a648a683855
ypark0615 <ypark0615hp@gmail.com>
2025-12-03 14:00:28 +0900

    초기 커밋.

---
+++ GitDockPanelSuite/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+//using WeifenLuo.WinFormsUI.Docking.VisualStyles;
+
+namespace GitDockPanelSuite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
